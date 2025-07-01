af87ce62c08da18faf04b9298c26ebbc62c72d46
Ofacy <github@ofacy.com>
2025-07-01 20:47:47 +0200

    feat: add basic mesh and texture params

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,37 @@
 #include "ShaderProgram.hpp"
 #include "Scop.hpp"
 
-struct final_Vertex {
-	float position[2];
-	float color[3];
-};
-
 int main(int argc, char** argv)
 {
+	char	*mesh_path = NULL;
+	char	*texture_path = NULL;
+
 	if (argc < 2) {
-		std::cerr << "Usage: " << argv[0] << " <mesh_file> [-t <texture_path.bmp>]" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " [-t <texture_path.bmp>] <mesh_file>" << std::endl;
 		return -1;
 	}
-	Mesh mesh = MeshLoader::loadMesh(argv[1]);
+	while (*(++argv)) {
+		std::string arg = *argv;
+		if (arg == "-t") {
+			texture_path = *(++argv);
+			if (texture_path == nullptr) {
+				std::cerr << "Invalid -t usage!" << std::endl;
+				return 1;
+			}
+		}
+		else {
+			if (mesh_path) {
+				std::cerr << "Cannot specify more than one OBJ file!" << std::endl;
+				return 1;
+			}
+			mesh_path = *argv;
+		}
+	}
+	if (!mesh_path) {
+		std::cerr << "OBJ file needed!" << std::endl;
+		return 1;
+	}
+	Mesh mesh = MeshLoader::loadMesh(mesh_path);
 	GLContext &glContext = GLContext::getInstance();
 	try {
 		glContext.init();
@@ -28,16 +47,19 @@ int main(int argc, char** argv)
 		std::cerr << "Runtime error: " << e.what() << std::endl;
 		return -1;
 	}
-	Texture *texture = BMPLoader::loadBMP("assets/textures/qpupier.bmp");
-	if (!texture) {
-		std::cerr << "Failed to load texture." << std::endl;
-		return -1;
+	Texture *texture = nullptr;
+	if (texture_path) {
+		texture = BMPLoader::loadBMP(texture_path);
+		if (!texture) {
+			std::cerr << "Failed to load texture." << std::endl;
+			return -1;
+		}
+		else {
+			glActiveTexture(GL_TEXTURE0);
+			texture->bind();
+		}
 	}
-	else {
-		glActiveTexture(GL_TEXTURE0);
-		texture->bind();
-		glEnable(GL_DEPTH_TEST);
-	}
+	glEnable(GL_DEPTH_TEST);
 	Scop scop(mesh, texture ? true : false);
 	scop.start();
 	delete texture;
