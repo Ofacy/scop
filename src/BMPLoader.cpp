32c8f1e7727b0796734bd19285b522d362afd7bc
Ofacy <github@ofacy.com>
2026-02-24 17:34:01 +0100

    refactor: add defines in bmp loader

--- src/BMPLoader.cpp
+++ src/BMPLoader.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <fstream>
 
+# define BMP_OFFSET_IDENTIFIER0 0
+# define BMP_OFFSET_IDENTIFIER1 1
+# define BMP_OFFSET_WIDTH 18
+# define BMP_OFFSET_HEIGHT 22
+# define BMP_OFFSET_DATA_OFFSET 10
+# define BMP_OFFSET_BPP 28
+# define BMP_HEADER_SIZE 54
+
 Texture *BMPLoader::loadBMP(const char *filename) {
 
 	std::ifstream file(filename, std::ios::binary);
@@ -10,18 +18,18 @@ Texture *BMPLoader::loadBMP(const char *filename) {
 	}
 
 	// Read BMP header
-	char header[54];
-	file.read(header, 54);
-	if (file.gcount() != 54 || header[0] != 'B' || header[1] != 'M') {
+	char header[BMP_HEADER_SIZE];
+	file.read(header, sizeof(header));
+	if (file.gcount() != sizeof(header) || header[BMP_OFFSET_IDENTIFIER0] != 'B' || header[BMP_OFFSET_IDENTIFIER1] != 'M') {
 		return nullptr; // Not a valid BMP file
 	}
-	int width = *reinterpret_cast<int*>(&header[18]);
-	int height = *reinterpret_cast<int*>(&header[22]);
-	int dataOffset = *reinterpret_cast<int*>(&header[10]);
-	if (width <= 0 || height <= 0 || dataOffset < 54) {
+	int width = *reinterpret_cast<int*>(&header[BMP_OFFSET_WIDTH]);
+	int height = *reinterpret_cast<int*>(&header[BMP_OFFSET_HEIGHT]);
+	int dataOffset = *reinterpret_cast<int*>(&header[BMP_OFFSET_DATA_OFFSET]);
+	if (width <= 0 || height <= 0 || dataOffset < BMP_HEADER_SIZE) {
 		return nullptr; // Invalid dimensions or data offset
 	}
-	int bpp = *reinterpret_cast<short*>(&header[28]);
+	int bpp = *reinterpret_cast<short*>(&header[BMP_OFFSET_BPP]);
 	if (bpp != 24 && bpp != 32) {
 		return nullptr; // Only support 24-bit or 32-bit BMP files
 	}
